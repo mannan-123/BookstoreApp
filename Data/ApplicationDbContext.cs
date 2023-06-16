3e4fd2a6b0577a7d1b133ddf307fe34bcf7cf60a
Abdul Mannan <98333068+mannan-123@users.noreply.github.com>
2023-06-16 07:19:22 -0700

    DB Context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace BookstoreApp.Data
+﻿using BookstoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookstoreApp.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories{ get; set; }
     }
 }
