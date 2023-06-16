6bf5ece66e576421fd1d0afc39a9540eb685c28c
Abdul Mannan <98333068+mannan-123@users.noreply.github.com>
2023-06-16 04:06:08 -0700

    Add project files.

---
+++ Models/Catagory.cs
@@ -0,0 +1,10 @@
+﻿namespace BookstoreApp.Models
+{
+    public class Catagory
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
