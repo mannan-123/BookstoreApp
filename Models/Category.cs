5e762609957d472ed85c1532f78cb7f88df5d47b
Abdul Mannan <98333068+mannan-123@users.noreply.github.com>
2023-06-16 04:47:50 -0700

    Packages

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace BookstoreApp.Models
-{
-    public class Catagory
-    {
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace BookstoreApp.Models
+{
+    public class Category
+    {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
